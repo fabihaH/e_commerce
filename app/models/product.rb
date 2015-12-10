f46eeb44f3e5a9b8e0cf566c25f22da8cacc9716
fabihaH <you@example.com>
2015-12-10 09:09:26 -0500

    products and comments scaffold is created

---
+++ app/models/product.rb
@@ -0,0 +1,4 @@
+class Product < ActiveRecord::Base
+  attr_accessible :content, :name, :title
+  has_many :comments
+end
---
+++ db/migrate/20151209180216_add_product_id_to_comments.rb
@@ -0,0 +1,4 @@
+class AddProductIdToComments < ActiveRecord::Migration
+  def change
+  end
+end
